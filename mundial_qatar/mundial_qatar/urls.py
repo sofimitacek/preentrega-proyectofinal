a52e491912cb8d768cfac18f9d0b982c5ac0d6a3
Sofía Mitacek <sofiamitacek@gmail.com>
2022-09-07 19:37:28 -0300

    probando template

--- mundial_qatar/mundial_qatar/urls.py
+++ mundial_qatar/mundial_qatar/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mundial_qatar.mundialqatar.views import probandotemplate
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('probandotemplate/', probandotemplate)
 ]
