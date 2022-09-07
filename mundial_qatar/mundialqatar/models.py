11e676858855b59e75cfb103a5aedca7a0cb5175
Sofía Mitacek <sofiamitacek@gmail.com>
2022-09-07 15:32:59 -0300

    primera migracion

--- mundial_qatar/mundialqatar/models.py
+++ mundial_qatar/mundialqatar/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
 class Seleccion(models.Model):
-    pais = 
+    pais = models.CharField(max_length=128)
+
+
+class Estadio(models.Model):
+    ciudad = models.CharField(max_length=128)
+
+class Arbitro(models.Model):
+    nombre = models.CharField(max_length=128)
+    fecha_nacimiento = models.DateField()
+    nacionalidad = models.CharField(max_length=128)
