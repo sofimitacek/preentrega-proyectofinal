f8d82e7da3ee3ba83c276063991acc498d4c47b9
Sofía Mitacek <sofiamitacek@gmail.com>
2022-09-07 19:39:52 -0300

    probando template 2

--- mundial_qatar/mundialqatar/views.py
+++ mundial_qatar/mundialqatar/views.py
@@ -1,3 +1,16 @@
+from contextvars import Context
+from pipes import Template
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from mundialqatar.models import Seleccion, Estadio, Arbitro
 
-# Create your views here.
+def probandotemplate(self):
+    html1 = open("C:\\Users\\chofy\\OneDrive\\Documentos\\proyectosAA\\mundial\\mundial_qatar\\templates.html")
+    plantilla = Template(html1.read())
+    html1.close()
+    contexto1 = Context()
+    documento = plantilla.render(contexto1)
+
+    return HttpResponse(documento)
