517211da5c60b8c3e3e93a6010e3abf7f09cac71
Sofía Mitacek <sofiamitacek@gmail.com>
2022-09-07 20:24:05 -0300

    funciona el template

--- mundial_qatar/mundialqatar/views.py
+++ mundial_qatar/mundialqatar/views.py
@@ -1,16 +1,17 @@
 from contextvars import Context
 from pipes import Template
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context, loader
 from mundialqatar.models import Seleccion, Estadio, Arbitro
 
-def probandotemplate(self):
-    html1 = open("C:\\Users\\chofy\\OneDrive\\Documentos\\proyectosAA\\mundial\\mundial_qatar\\templates.html")
-    plantilla = Template(html1.read())
-    html1.close()
-    contexto1 = Context()
-    documento = plantilla.render(contexto1)
+def probandoTemplate(request):
+    html_1 = open("C:/Users/chofy/OneDrive/Documentos/proyectosAA/mundial/mundial_qatar/templates/plantilla.html")
+    lista_selecciones = Seleccion.objects.all()
+    diccionario = {'selecciones': lista_selecciones}
+    plantilla = Template(html_1.read())
+    html_1.close()
+    contexto_1 = Context(diccionario)
+    documento = plantilla.render(contexto_1)
 
     return HttpResponse(documento)
