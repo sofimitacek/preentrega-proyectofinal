b39fd24fd1713430985098a51270ef671d23ea40
ZarzaEdu <zarzaedu@gmail.com>
2022-09-12 10:41:15 -0300

    Agregado de formulario en app Selecciones

---
+++ Selecciones/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms  import UserCreationForm
+from django.contrib.auth.models import User
+
+class Player_form (forms.Form):
+    First_name= forms.CharField(max_length=30)
+    Second_name= forms.CharField(max_length=30)
+    club= forms.CharField(max_length=60)
+    shirt_number= forms.IntegerField(max_length=2)
+    height= forms.FloatField()
+    age= forms.FloatField()
+
+class Coachs_Staff (forms.Form):
+    first_name= forms.CharField(max_length=30)
+    second_name= forms.CharField(max_length=30)
+    age= forms.FloatField()
+    work_area= forms.CharField(max_length=60)
