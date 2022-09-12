58662168e771538399b4e54a9d222b5e9288a96c
ZarzaEdu <zarzaedu@gmail.com>
2022-09-12 09:14:13 -0300

    Creación de modelos Players y Coachs_Staff en la app Selecciones

--- Selecciones/models.py
+++ Selecciones/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.db import models 
+
+class Players (models.Model):
+    First_name= models.CharField(max_length=30)
+    Second_name= models.CharField(max_length=30)
+    club= models.CharField(max_length=60)
+    shirt_number= models.IntegerField(max_length=2)
+    height= models.FloatField()
+    age= models.FloatField()
+
+class Coachs_Staff (models.Model):
+    first_name= models.CharField(max_length=30)
+    second_name= models.CharField(max_length=30)
+    age= models.FloatField()
+    work_area= models.CharField(max_length=60)
+
+
+
 
 # Create your models here.
